95b9d2142096c46e4ee8ff68a3629ae1f4517ec5
Hashin Mohammed <hashinpks@gmail.com>
2023-03-02 13:32:50 +0530

    book edit with form

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function BookEdit() {
+function BookEdit({book}) {
+  const [title,setTitle]=useState(book.title)
+const handleInputChange=(event)=>{
+  setTitle(event.target.value)
+}
+const handleSubmit=(event)=>{
+  event.preventDefault()
+}
   return (
-    <div>BookEdit</div>
+   <form onSubmit={handleSubmit} className="book-edit">
+    <label>Title</label>
+    <input onChange={handleInputChange} value={title} type="text" className="input" />
+    <buttton className="button is-primary">Save</buttton>
+
+   </form>
   )
 }
 
